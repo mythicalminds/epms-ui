ed78b86044cea52326acfc7aa73e7458fbcd8370
mythicalminds <38314839+mythicalminds@users.noreply.github.com>
2018-04-12 16:17:32 +0530

    inital commit for master

---
+++ src/main/java/com/siemens/epms/ui/EpmsUiApplication.java
@@ -0,0 +1,12 @@
+package com.siemens.epms.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpmsUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpmsUiApplication.class, args);
+	}
+}
